3ed53a4f96afa2712bcce8c65f9a897a91c0cada
srikanth <srikanthpoojary63@gmail.com>
2017-11-10 10:09:30 +0530

    hotel single listing page

---
+++ resources/views/hotels/hotels-details-single.blade.php
@@ -0,0 +1,15 @@
+extends('layouts.app')
+
+@section('content')
+ <div class="container">
+ 	<div class="row">
+ 		<div class="hotel_booking_info">
+      div class="container">
+      	<div class="row">
+      		<div class="hotel_booking_info">
+
+      		</div>
+      	</div>
+ 		</div>
+ 	</div>
+</div>
