a9b3e2b77c8d1866f833941f0312d687922ceea6
srikanth <srikanthpoojary63@gmail.com>
2017-12-22 17:31:05 +0530

    terms and privcay policy page

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="clearfix"></div>
+        <!-- Header Container / End -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Privacy Policy</h1>
+          <p>
+            As a registered member of pavanyatra.com, you will be entitled to savings and benefits on airfares, Bus Ticket Booking, hotel reservations, holiday packages and car rentals, among other travel services.
+          </p>
+          <p>
+            In addition, look forward to receiving monthly newsletters and exclusive promotions offering special deals. That's why we've provided this Privacy Policy, which sets forth our policies regarding the collection, use and protection of the personal information of those using the pavanyatra.com website. Personal information means information that can be linked to a specific individual, such as name, address, telephone number, e - mail address, and credit card number.
+          </p>
+          <p>
+            We encourage you to review our Privacy Policy, and become familiar with it, but you should know that we do not sell or rent our customers' personal information to third parties. Please note that we review our Privacy Policy from time to time, and we may make periodic changes to the policy in connection with that review. Therefore, you may wish to bookmark this page and/or periodically review this page to make sure you have the latest version.
+          </p>
+          <p>
+            Regardless of later updates, we will abide by the privacy practices described to you in this Privacy Policy at the time you provided us with your personal information.
+          </p>
+        </div>
+      </div>
+
+    </div>
+</div>
+@endsection
---
+++ resources/views/terms.blade.php
@@ -0,0 +1,154 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="clearfix"></div>
+        <!-- Header Container / End -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Terms and Conditions</h1>
+          <p>
+            Pavanyatra reserves the right to charge listing fees for certain listings, as well as transaction fees based on certain completed transactions using the services. Pavanyatra further reserves the right to alter any and all fees from time to time, without notice.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Usage Of The Mobile Number Of The User By pavanyatra</h1>
+          <p>
+            Pavanyatra may send booking confirmation & itinerary information & update the customer on the booking status and any further itinerary information via SMS (short messaging service) on the mobile number given by the customer at the time of booking. The customer hereby unconditionally consents such intimation via SMS by pavanyatra in accordance with the 'Do not disturb' guidelines of Telecom Regulatory Authority of India (TRAI) or such other authority in India and abroad.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Onus Of The User</h1>
+          <p>
+            <ul>
+              <li>
+                 Pavanyatra is responsible only for the transactions that are done by the User through pavanyatra. Pavanyatra will not be responsible for screening, censoring or otherwise controlling transactions, including whether the transaction is legal and valid as per the laws of the land of the User.
+              </li>
+              <li>
+                The User warrants that they will abide by all such additional procedures and guidelines, as modified from time to time, in connection with the use of the services. The User further warrants that they will comply with all applicable laws and regulations regarding use of the services with respect to the jurisdiction concerned for each transaction
+              </li>
+              <li>
+                The User represent and confirm that the User is of legal age to enter into a binding contract and is not a person barred from availing the Services under the laws of India or other applicable law.
+              </li>
+            </ul>
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Advertisers on pavanyatra or linked websites</h1>
+          <p>
+             Pavanyatra is not responsible for any errors, omissions or representations on any of its pages or on any links or on any of the linked website pages. Pavanyatra does not endorse any advertiser on its web pages in any manner. The Users are requested to verify the accuracy of all information on their own before undertaking any reliance on such information.
+          </p>
+          <p>
+            The linked sites are not under the control of pavanyatra and pavanyatra is not responsible for the contents of any linked site or any link contained in a linked site, or any changes or updates to such sites. Pavanyatra is providing these links to the Users only as a convenience and the inclusion of any link does not imply endorsement of the site by pavanyatra.com.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Insurance</h1>
+          <ul>
+            <li>
+               Unless explicitly provided by pavanyatra in any specific service or deliverable, obtaining sufficient insurance coverage is the obligation/option of the user and pavanyatra doesn't accept any claims arising out of such scenarios.
+            </li>
+            <li>
+              Insurance, if any provided as a part of the service/ product by pavanyatra shall be as per the terms and conditions of the insuring company. The User shall contact the insurance company directly for any claims or disputes and pavanyatra shall not provide any express or implied undertakings for acceptance of the claims by the insurance company.
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Force Majure Circumstances</h1>
+          <p>
+             The user agrees that there can be exceptional circumstances where the service operators like the airlines, hotels, the respective transportation providers or concerns may be unable to honor the confirmed bookings due to various reasons like climatic conditions, labor unrest, insolvency, business exigencies, government decisions, operational and technical issues, route and flight cancellations etc. If pavanyatra is informed in advance of such situations where dishonor of bookings may happen, it will make its best efforts to provide similar alternative to its customers or refund the booking amount after reasonable service charges, if supported and refunded by that respective service operators. The user agrees that pavanyatra being an agent for facilitating the booking services shall not be responsible for any such circumstances and the customers have to contact that service provider directly for any further resolutions and refunds.
+          </p>
+          <p>
+            The User agrees that in situations due to any technical or other failure in pavanyatra, services committed earlier may not be provided or may involve substantial modification. In such cases, pavanyatra shall refund the entire amount received from the customer for availing such services minus the applicable cancellation, refund or other charges, which shall completely discharge any and all liabilities of pavanyatra against such non-provision of services or deficiencies. Additional liabilities, if any, shall be borne by the User.
+          </p>
+          <p>
+            Pavanyatra shall not be liable for delays or inabilities in performance or nonperformance in whole or in part of its obligations due to any causes that are not due to its acts or omissions and are beyond its reasonable control, such as acts of God, fire, strikes, embargo, acts of government, acts of terrorism or other similar causes, problems at airlines, rails, buses, hotels or transporters end. In such event, the user affected will be promptly given notice as the situation permits
+          </p>
+          <p>
+            Without prejudice to whatever is stated above, the maximum liability on part of pavanyatra arising under any circumstances, in respect of any services offered on the site, shall be limited to the refund of total amount received from the customer for availing the services less any cancellation, refund or others charges, as may be applicable. In no case the liability shall include any loss, damage or additional expense whatsoever beyond the amount charged by pavanyatra for its services.
+          </p>
+          <p>
+             In no event shall pavanyatra and/or its suppliers be liable for any direct, indirect, punitive, incidental, special, consequential damages or any damages whatsoever including, without limitation, damages for loss of use, data or profits, arising out of or in any way connected with the use or performance of the pavanyatra website(s) or any other channel . Neither shall pavanyatra be responsible for the delay or inability to use the pavanyatra websites or related services, the provision of or failure to provide services, or for any information, software, products, services and related graphics obtained through the pavanyatra website(s), or otherwise arising out of the use of the pavanyatra website(s), whether based on contract, tort, negligence, strict liability or otherwise.
+          </p>
+          <p>
+            Pavanyatra is not responsible for any errors, omissions or representations on any of its pages or on any links or on any of the linked website pages.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Force Major Circumstances</h1>
+          <p>
+            The user agrees that there can be exceptional circumstances where the service operators like the airlines, hotels, the respective transportation providers or concerns may be unable to honor the confirmed bookings due to various reasons like climatic conditions, labor unrest, insolvency, business exigencies, government decisions, operational and technical issues, route and flight cancellations etc. If pavanyatra is informed in advance of such situations where dishonor of bookings may happen, it will make its best efforts to provide similar alternative to its customers or refund the booking amount after reasonable service charges, if supported and refunded by that respective service operators. The user agrees that pavanyatra being an agent for facilitating the booking services shall not be responsible for any such circumstances and the customers have to contact that service provider directly for any further resolutions and refunds.
+          </p>
+          <p>
+            The User agrees that in situations due to any technical or other failure in pavanyatra, services committed earlier may not be provided or may involve substantial modification. In such cases, pavanyatra shall refund the entire amount received from the customer for availing such services minus the applicable cancellation, refund or other charges, which shall completely discharge any and all liabilities of pavanyatra against such non-provision of services or deficiencies. Additional liabilities, if any, shall be borne by the User.
+          </p>
+          <p>
+             Pavanyatra shall not be liable for delays or inabilities in performance or nonperformance in whole or in part of its obligations due to any causes that are not due to its acts or omissions and are beyond its reasonable control, such as acts of God, fire, strikes, embargo, acts of government, acts of terrorism or other similar causes, problems at airlines, rails, buses, hotels or transporters end. In such event, the user affected will be promptly given notice as the situation permits
+          </p>
+          <p>
+             Without prejudice to whatever is stated above, the maximum liability on part of pavanyatra arising under any circumstances, in respect of any services offered on the site, shall be limited to the refund of total amount received from the customer for availing the services less any cancellation, refund or others charges, as may be applicable. In no case the liability shall include any loss, damage or additional expense whatsoever beyond the amount charged by pavanyatra for its services.
+          </p>
+          <p>
+             In no event shall pavanyatra and/or its suppliers be liable for any direct, indirect, punitive, incidental, special, consequential damages or any damages whatsoever including, without limitation, damages for loss of use, data or profits, arising out of or in any way connected with the use or performance of the pavanyatra website(s) or any other channel . Neither shall pavanyatra be responsible for the delay or inability to use the pavanyatra websites or related services, the provision of or failure to provide services, or for any information, software, products, services and related graphics obtained through the pavanyatra website(s), or otherwise arising out of the use of the pavanyatra website(s), whether based on contract, tort, negligence, strict liability or otherwise.
+          </p>
+          <p>
+            Pavanyatra is not responsible for any errors, omissions or representations on any of its pages or on any links or on any of the linked website pages.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Safety Of Data Downloaded</h1>
+          <p>
+            The User understands and agrees that any material and/or data downloaded or otherwise obtained through the use of the Service is done entirely at their own discretion and risk and they will be solely responsible for any damage to their computer systems or loss of data that results from the download of such material and/or data..
+          </p>
+          <p>
+             Nevertheless, pavanyatra will always make its best endeavors to ensure that the content on its websites or other information channels are free of any virus or such other malwares.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Feedback From Customer And Solicitation</h1>
+          <p>
+            The customer is aware that pavanyatra. Com provides various services like hotel bookings, car rentals, holidays and would like to learn about them, to enhance his / her travel experience.
+          </p>
+          <p>
+            The customer hereby specifically authorizes pavanyatra.com to contact the user with offers on various services offered by it through direct mailers, e-mailers, telephone calls, short messaging services (SMS) or any other medium, from time to time. In case that the customer chooses not to be contacted, he / shall write to pavanyatra.com for specific exclusion at Customercare@pavanyatra.com
+          </p>
+          <p>
+            The customers are advised to read and understand the privacy policy of pavanyatra on its website in accordance of which pavanyatra contacts, solicits the user or shares the user's information.
+          </p>
+
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Proprietary Rights</h1>
+          <p>
+            pavanyatra may provide the User with contents such as sound, photographs, graphics, video or other material contained in sponsor advertisements or information. This material may be protected by copyrights, trademarks, or other intellectual property rights and laws.
+          </p>
+          <p>
+            The User may use this material only as expressly authorized by pavanyatra and shall not copy, transmit or create derivative works of such material without express authorization. The User acknowledges and agrees that he/she shall not upload post, reproduce, or distribute any content on or through the Services that is protected by copyright or other proprietary right of a third party, without obtaining the written permission of the owner of such right.
+          </p>
+          <p>
+            Any copyrighted or other proprietary content distributed with the consent of the owner must contain the appropriate copyright or other proprietary rights notice. The unauthorized submission or distribution of copyrighted or other proprietary content is illegal and could subject the User to personal liability or criminal prosecution
+          </p>
+        </div>
+      </div>
+    </div>
+ </div>
+@endsection
