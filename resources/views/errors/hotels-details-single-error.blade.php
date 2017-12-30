81a358b43af252739f22ccbeab8a46fc66739917
srikanth <srikanthpoojary63@gmail.com>
2017-12-30 14:44:16 +0530

    added all the extra files like error pages css and js

---
+++ resources/views/errors/hotels-details-single-error.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="row">
+    <div class="container" style="min-height:300px;margin-top:30px;">
+      <div class="hotel_booking_info">
+          <div class="clearfix"></div>
+       </div>
+       <div class="row">
+         <div class="col-lg-12">
+           <h1>Sorry! This Hotel Is Not Available For Some Time</h1><br />
+           <h3>Please Try Again Or Search Other Hotels</h3>
+         </div>
+       </div>
+    </div>
+   </div>
+@endsection
